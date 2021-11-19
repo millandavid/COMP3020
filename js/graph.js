b5397594042958dccf784cdcd599ca9863d56738
millandavid <71908868+millandavid@users.noreply.github.com>
2021-11-18 19:11:41 -0600

    graph json

--- js/graph.js
+++ js/graph.js
@@ -1,30 +1,47 @@
 // graph
 
+var xmlhttp = new XMLHttpRequest();
+var url = "http://127.0.0.1:5500/js/data.json";
+xmlhttp.open("GET",url,true);
+xmlhttp.send();
+xmlhttp.onreadystatechange = function(){
+  if(this.readyState == 4 && this.status == 200){
+    var data = JSON.parse(this.responseText);
+    //console.log(data);
+    var days = data.november_calories.map(function(elem){
+      return elem.date;
+    });
+    //console.log(days)
+    var calories = data.november_calories.map(function(elem){
+      return elem.calories;
+    });
 
-
-const ctx = document.getElementById('canvas').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Nov 1','Nov 2','3','4','5','6'],
-        datasets: [{
-            label: 'Calories per Day',
-            data: [12, 11, 3, 5, 2, 3],
-            backgroundColor: 'transparent',
-            borderColor: 'red',
-            borderWidth: 4
-        }]
-    },
-    options: {
-      element:{
-        line:{
-          tension:0
-        }
-      },
-        scales: {
-            y: {
-                beginAtZero: true
+    const ctx = document.getElementById('canvas').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: days,
+            datasets: [{
+                label: 'Calories per Day',
+                data: calories,
+                backgroundColor: 'transparent',
+                borderColor: 'red',
+                borderWidth: 4
+            }]
+        },
+        options: {
+          element:{
+            line:{
+              tension:0
+            }
+          },
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
         }
-    }
-});
+    });
+  }
+}
+
